7425de97c2e52e710faeb520ce714e91bd50ab9e
David Ros <davidros.dev@proton.me>
2023-10-10 00:39:41 +0200

    Minor Changes

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -4,3 +4,36 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var dataText = ["Frontend Developer", "UI/UX Designer"];
+
+  function typeWriter(text, i, fnCallback) {
+    if (i < text.length) {
+      document.querySelector(".section__text__p2").innerHTML =
+        text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback);
+      }, 100);
+    } else if (typeof fnCallback == "function") {
+      setTimeout(fnCallback, 700);
+    }
+  }
+
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == "undefined") {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 500);
+    }
+
+    if (i < dataText[i].length) {
+      typeWriter(dataText[i], 0, function () {
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+
+  StartTextAnimation(0);
+});
